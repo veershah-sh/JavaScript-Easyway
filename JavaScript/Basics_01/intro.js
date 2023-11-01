f770e035afef2463cc30b5d6a7648a40344e3a92
Veer Shah <42229094+veershah-sh@users.noreply.github.com>
2023-11-01 13:33:04 +0000

    basics js

--- JavaScript/Basics_01/intro.js
+++ JavaScript/Basics_01/intro.js
@@ -1,11 +1,45 @@
+// import alert from "alert"
+// io
+
+// dt var = val;
+
+// const, let, var -> declare variable
+// const, let -> ES6
+// const -> constant
+const pi = 3.14
+
+// let -> variable
+let name = "devanshu"
+
+// var -> variable
+var num = 10
+
 const password = "12345"
 let age = 20
 var no = 10
 // NOTE: DO NOT USE VAR 
 
-console.log(password)
-console.table([age, no])
+// console.log(pi)
+// console.log(name)
+// console.log(num)
+// console.log(password)
+// console.log(age)
+// console.log(no)
+
+// console.table([pi, name, num])
+// console.table([password, age, no])
+
+if(true){
+    let no1 = 10;
+    const no2 = 20;
+    var no3 = 30;
+}
+// console.log(no3);
 
 
 // let alert = require('alert'); 
 // alert("message")
+
+// html -> skleton
+// css -> muscels, skin
+// js -> functions
