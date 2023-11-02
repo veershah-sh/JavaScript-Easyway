62b9f1e0e26ca4e5e613ee0059aaffb84be4eada
Veer Shah <42229094+veershah-sh@users.noreply.github.com>
2023-11-02 13:55:49 +0000

    added in basics01

--- JavaScript/Basics_01/datatype.js
+++ JavaScript/Basics_01/datatype.js
@@ -1,26 +1,52 @@
 "use strict"; // treats all js files as latest version
 
-let name = "name"
-let age = 18
-let isLoggedIn = false
-let state;
+let name = "name" // string
+let age = 18 // number
+let pi = 3.14 // number
+let isLoggedIn = false // boolean (true, false)(1,0)
+let state; // undefined
+let marks = null; // null
+
+const age1 = "18"
+
+// console.log(age1);
 
 // number => 2 to power 53
 // bigint
-// string => ""
-// boolean => true/false
+
+// string => "abcd"
+
+// boolean => true/false (1,0)
+
 // null => standalone value / object
+
 // undefined => undefined
+
 // symbol => unique
+
+// array 
+// object
 
 
 // object
 
-console.log(typeof(undefined)); // undefined
+// console.log(typeof(age1));
+// console.log(typeof(age));
+// console.log(typeof(pi));
+
+// console.log(typeof(undefined)); // undefined
+
 console.log(typeof null); // object
-console.log(typeof "4444"); // object
-console.log(12 + 156); // object
+
+console.log(typeof "4444"); // string
+
+console.log(12 + 156); // number
 let makrs = 156
+
+// validation for data type
 if(typeof(makrs) == "number"){
     console.log(12 + makrs)
+}
+else{
+    console.log('not a number');
 }
--- JavaScript/Basics_01/operations.js
+++ JavaScript/Basics_01/operations.js
@@ -7,30 +7,59 @@ let negValue = -value
 // console.log(2+2);
 // console.log(2-2);
 // console.log(2*2);
-// console.log(2**3);
+
+// // quotient
 // console.log(2/3);
+
+// // reminder
 // console.log(2%3);
+
+// // ** -> exponential
+// console.log(2**3); // 2*2*2 = 8
+
+// =======================================================
 
 let str1 = "hello"
 let str2 = " name"
 
-let str3 = str1 + str2
+let str3 = str1 + str2 + " Good night"
+
 // console.log(str3);
+
 
 // console.log("1" + 2);
 // console.log(1 + "2");
+
 // console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+
+// console.log(1 + 2 + "2"); // 32
+// console.log(1 + 2 + "2" + 2 + 5); // 3225
+// console.log((1 + 2) + "2" + (2 + 5)); // 327
 
 // console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+// console.log(+true); // 1 
+// console.log(+false); // 0
+
+// console.log(true); // true 
+// console.log(false); // false
+
+// console.log(+"111"); // 111
+// console.log(+" 111"); // 0
+// console.log(+"sdsd"); // nan -> not a number
+// console.log(+" "); // 0
+// console.log(+""); // 0
 
 let num1, num2, num3
 
 num1 = num2 = num3 = 2 + 2
 
 let gameCounter = 100
-++gameCounter;
+// ++gameCounter;
+console.log(gameCounter++); // post increment
 console.log(gameCounter);
+console.log(++gameCounter); // per increment
+
+console.log(gameCounter--); // post decrement
+console.log(gameCounter);
+console.log(--gameCounter); // per decrement
