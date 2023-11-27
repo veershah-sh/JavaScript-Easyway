ebf32d17df4c0d77e1ce86360af175d36ad1f39c
Veer Shah <42229094+veershah-sh@users.noreply.github.com>
2023-11-27 13:34:09 +0000

    map filter

--- JavaScript/basics_02/arrFun.js
+++ JavaScript/basics_02/arrFun.js
@@ -51,8 +51,74 @@ let items = [
     }
 ]
 
-items.forEach((item) => {
-    if(item.type === "veg"){
-        console.log(item);
+// items.forEach((item) => {
+//     if(item.type === "veg"){
+//         console.log(item);
+//     }
+// })
+
+
+// let num = nums.forEach((num) => {
+//     console.log(num)
+//     return num
+// })
+
+// console.log(num)
+
+// map
+
+let item = items.map((i) => {
+    if(i.type === "veg"){
+        // console.log(item);
+        return i
     }
 })
+// console.log(item);
+// total -> m1,m2,m3
+// per -> total/3
+
+
+// filter
+
+let itemsFilter = items.filter((item) => item.type === "veg" && item.taste !== "Spicy" && item.price <= 80)
+// console.log(itemsFilter);
+
+let cloth_items = [
+    {
+        type: "Shirt",
+        size: "s",
+        fashion: "M",
+        brand: "Denim",
+        price: 2999
+    },
+    {
+        type: "Tshirt",
+        size: "m",
+        fashion: "F",
+        brand: "DG",
+        price: 1888
+    },
+    {
+        type: "Pent",
+        size: "xl",
+        fashion: "F",
+        brand: "Denim",
+        price: 3888
+    },
+    {
+        type: "Shorts",
+        size: "s",
+        fashion: "M",
+        brand: "UM",
+        price: 1555
+    }
+]
+
+let clothFilter = cloth_items.filter((ci) => ci.size === "s")
+                            .filter((ci) => ci.brand === "Denim")
+
+// console.log(clothFilter);
+
+let item1 = items.map((item) => {return item})
+            .filter((item) => item.type === "veg")
+console.log(item1)
