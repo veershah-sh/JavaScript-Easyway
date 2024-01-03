5859c4d194d6301b4e135c2f4a6c89eb26a55070
Veer Shah <42229094+veershah-sh@users.noreply.github.com>
2024-01-03 20:10:17 +0530

    Create call.js

---
+++ JavaScript/oop/call.js
@@ -0,0 +1,13 @@
+function setUsername(username){
+    this.username = username
+    console.log("called");
+}
+
+function createUser(username, email, password){
+    setUsername.call(this, username)
+    this.email = email
+    this.password = password
+}
+
+let veer = new createUser("veer", "veer@google.com", "123")
+console.log(veer)
