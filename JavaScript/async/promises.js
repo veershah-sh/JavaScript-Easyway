e75d4bcebbcf878b3ea2412a70b8e86464137ec7
Veer Shah <42229094+veershah-sh@users.noreply.github.com>
2024-01-05 11:33:58 +0000

    more promises

--- JavaScript/async/promises.js
+++ JavaScript/async/promises.js
@@ -5,12 +5,69 @@
 // function, method, callback, handler, executor
 
 // creating promise
-let onePromise = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        console.log('Promise one completed');
+// let DBCall = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         console.log("promise executed...")
+//         resolve()
+//     },2000)
+// })
+
+// DBCall.then(function(){
+//     console.log("promise resolved...");
+// })
+
+let Challenge = new Promise(function(resolve,reject){
+    let error = false
+    if(!error){
         resolve()
-    }, 1000)
+    }
+    else{
+        reject("Challenge not accepted.")
+    }
 })
+
+// Challenge
+// .then(() => {
+//     console.log("Challenge Accepted!");
+// })
+// .catch((error) => {
+//     console.log(`Error: ${error}`);
+// })
+
+
+let GetUser = new Promise(function(resolve,reject){
+    let error = false
+    if(!error){
+        resolve({username: "abc", password:"abc123"})
+    }
+    else{
+        reject("Query not executed.")
+    }
+})
+
+function matchUserData(username){
+    if(username === "abc"){
+        console.log(`${username} verified`);
+    }
+}
+GetUser.then(function(user){
+    return user.username
+})
+.then(matchUserData)
+.catch(function(error){
+    console.log(`Error: ${error}`);
+})
+.finally(function(){
+    console.log("Promise executed...");
+})
+
+// creating promise
+// let onePromise = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         console.log('Promise one completed');
+//         resolve()
+//     }, 1000)
+// })
 
 // consume promise
 // then(), catch(), finally()
@@ -37,27 +94,27 @@ let onePromise = new Promise(function(resolve, reject){
 // })
 
 
-let promiseThree = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        let error = false
-        if(!error){
-            resolve({username:"abc123", password:"12345"})
-        }
-        else{
-            reject("ERROR: The promise request failed!")
-        }
-    },2000)
-})
+// let promiseThree = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         let error = false
+//         if(!error){
+//             resolve({username:"abc123", password:"12345"})
+//         }
+//         else{
+//             reject("ERROR: The promise request failed!")
+//         }
+//     },2000)
+// })
 
 
 // thenables
-promiseThree.then(function(userData){
-    console.log(userData);
-    return userData.username
-}).then(function(unmae){
-    console.log(unmae);
-}).catch(function(err){
-    console.log(err);
-}).finally(function(){
-    console.log("Promise executed successfully with resolve or reject")
-})
+// promiseThree.then(function(userData){
+//     console.log(userData);
+//     return userData.username
+// }).then(function(unmae){
+//     console.log(unmae);
+// }).catch(function(err){
+//     console.log(err);
+// }).finally(function(){
+//     console.log("Promise executed successfully with resolve or reject")
+// })
